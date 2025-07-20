c5e7a6bf52e4852089f6bb87811846ed11c7aef5
R Akhrom Darmawan <rahmat.darmawan16@student.uisi.ac.id>
2025-07-20 15:09:34 +0700

    update

--- cypress/e2e/ORM_0.0.5-Add_admin/add_admin.cy.js
+++ cypress/e2e/ORM_0.0.5-Add_admin/add_admin.cy.js
@@ -12,26 +12,26 @@ describe("TC-ADMIN-001 - Add User di OrangeHRM", () => {
     // Navigate to Admin section
     cy.get("button").contains("Add").click();
 
-    // Click on Add button to open the form
+    // select user role
     cy.get(".oxd-select-text").eq(0).click();
     cy.get(".oxd-select-option").contains("ESS").click();
 
-    // Select user role as ESS
+    // Select user name auto-complete
     cy.get("input[placeholder='Type for hints...']").type(
       "Timothy Lewis Amiano"
     );
     cy.wait(1000);
     cy.get(".oxd-autocomplete-option").contains("Timothy Lewis Amiano").click();
 
-    // Select employee from the dropdown
+    // Select employee active
     cy.get(".oxd-select-text").eq(1).click();
     cy.get(".oxd-select-option").contains("Enabled").click();
 
-    // Set status to Enabled
+    //select new username
     const newUsername = `testuser${Date.now()}`;
     cy.get(".oxd-input").eq(1).type(newUsername);
 
-    // Input new username
+    // Input new password
     cy.get('input[type="password"]').eq(0).type("Password123!");
     cy.get('input[type="password"]').eq(1).type("Password123!");
 
